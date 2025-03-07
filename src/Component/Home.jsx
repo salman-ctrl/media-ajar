d0a05e2ec3664428f0e75514c5841b1abdf914cf
salman-ctrl <zirmanvictory@gmail.com>
2025-03-07 23:40:21 +0700

    edit card

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -16,10 +16,7 @@ const Home = () => {
                         <FaPlay />
                     </div>
 
-                    <div className='p-6 flex flex-col items-center text-center'>
-                        <h2 className='text-lg font-semibold text-white'>Mari Mengenal Warisan Budaya di Sekitar Kita</h2>
 
-                    </div>
                 </div>
             </div>
         </div>
