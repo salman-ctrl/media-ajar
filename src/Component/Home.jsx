1462c715304edb469076e1f765caea64bb807c24
salman-ctrl <zirmanvictory@gmail.com>
2025-03-07 23:41:39 +0700

    add navbar from home

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
             className="h-screen w-screen bg-cover bg-center overflow-hidden flex flex-col"
             style={{ backgroundImage: `url(${bg})` }}
         >
+            <Navbar />
             <div className='w-full h-full flex justify-center items-center'>
                 <div className=' h-80 relative w-96 bg-amber-600 rounded-2xl shadow-2xl overflow-hidden flex flex-col hover:scale-110 transition-all duration-700'>
                     <img src={bg} alt="" className='h-50 w-full object-cover relative' />
