928fc9f46ce04b8d92ca332d27e820a464bf0f94
salman-ctrl <zirmanvictory@gmail.com>
2025-03-07 23:38:33 +0700

    edit halaman home

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -12,8 +12,11 @@ const Home = () => {
             <div className='w-full h-full flex justify-center items-center'>
                 <div className='h-80 w-96 bg-amber-600 rounded-2xl shadow-2xl overflow-hidden flex flex-col hover:scale-110 transition-all duration-700'>
                     <img src={bg} alt="" className='h-50 w-full object-cover relative' />
-                    <p className='absolutes m-auto'><FaPlay />
-                    </p>
+                    <div>
+                        <FaPlay />
+
+                    </div>
+
                     <div className='p-6 flex flex-col items-center text-center'>
                         <h2 className='text-lg font-semibold text-white'>Mari Mengenal Warisan Budaya di Sekitar Kita</h2>
 
