d63849729a3af08a8625d76cba5a1a0e5e0abcd9
salman-ctrl <zirmanvictory@gmail.com>
2025-03-07 23:41:22 +0700

    delete navbar from home

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -8,11 +8,11 @@ const Home = () => {
             className="h-screen w-screen bg-cover bg-center overflow-hidden flex flex-col"
             style={{ backgroundImage: `url(${bg})` }}
         >
-            <Navbar />
             <div className='w-full h-full flex justify-center items-center'>
                 <div className=' h-80 relative w-96 bg-amber-600 rounded-2xl shadow-2xl overflow-hidden flex flex-col hover:scale-110 transition-all duration-700'>
                     <img src={bg} alt="" className='h-50 w-full object-cover relative' />
                     <div className='absolutes inset-0 flex justify-center'>
+                        <FaPlay />
                     </div>
 
                     <div className='p-6 flex flex-col items-center text-center'>
