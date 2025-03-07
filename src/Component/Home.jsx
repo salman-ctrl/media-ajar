c424babb79bfe69c6bf7c18044ea9b1701638489
salman-ctrl <zirmanvictory@gmail.com>
2025-03-07 23:37:37 +0700

    tambah halaman navbar edit

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import Navbar from './Navbar'
-
+import React from 'react';
+import Navbar from './Navbar';
+import bg from '../assets/bg.jpg';
+import { FaPlay } from "react-icons/fa";
 const Home = () => {
     return (
-        <div>
+        <div
+            className="h-screen w-screen bg-cover bg-center overflow-hidden flex flex-col"
+            style={{ backgroundImage: `url(${bg})` }}
+        >
             <Navbar />
+            <div className='w-full h-full flex justify-center items-center'>
+                <div className='h-80 w-96 bg-amber-600 rounded-2xl shadow-2xl overflow-hidden flex flex-col hover:scale-110 transition-all duration-700'>
+                    <img src={bg} alt="" className='h-50 w-full object-cover relative' />
+                    <p className='absolutes m-auto'><FaPlay />
+                    </p>
+                    <div className='p-6 flex flex-col items-center text-center'>
+                        <h2 className='text-lg font-semibold text-white'>Mari Mengenal Warisan Budaya di Sekitar Kita</h2>
+
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div className='h-[10vh] w-screen bg-amber-400'>
-            <ul>
+        <div className='h-[10vh] w-screen bg-amber-600 text-white text-2xl shadow-2xl flex'>
+
+            <ul className='flex gap-x-10 w-full justify-end pr-10 items-center'>
                 <li>
                     <Link to='/'>Home</Link>
                 </li>
