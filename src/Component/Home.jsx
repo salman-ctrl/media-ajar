720e221fa17d2bc9197d76a88420d3ef9b182acc
salman-ctrl <zirmanvictory@gmail.com>
2025-03-07 23:40:00 +0700

    edit halaman home

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -10,11 +10,10 @@ const Home = () => {
         >
             <Navbar />
             <div className='w-full h-full flex justify-center items-center'>
-                <div className='h-80 w-96 bg-amber-600 rounded-2xl shadow-2xl overflow-hidden flex flex-col hover:scale-110 transition-all duration-700'>
+                <div className=' h-80 relative w-96 bg-amber-600 rounded-2xl shadow-2xl overflow-hidden flex flex-col hover:scale-110 transition-all duration-700'>
                     <img src={bg} alt="" className='h-50 w-full object-cover relative' />
-                    <div>
+                    <div className='absolutes inset-0 flex justify-center'>
                         <FaPlay />
-
                     </div>
 
                     <div className='p-6 flex flex-col items-center text-center'>
